6e9f72b7cb81d7b74c2d29f0e4eb9bdf10f6d6eb
TheAnosmic <theanosmic@gmail.com>
2015-08-13 23:11:43 +0300

    We now create enemies with an Enemy Factory!

--- Assets/Scripts/EnemyFactory.cs
+++ Assets/Scripts/EnemyFactory.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyFactory : MonoBehaviour {
+public class EnemyFactory {
+    public GameObject Target { get; set; }
+    public GameObject[] PossibleEnemies { get; set; }
+    private GameObject m_producer;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public EnemyFactory(GameObject producer)
+    {
+        m_producer = producer;
+    }
+
+    public void Create(int count, Vector2 topLeft, Vector2 bottomRight)
+    {
+        var enemiesCount = PossibleEnemies.Length;
+        if (enemiesCount > 0)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var randomEnemy = Random.Range(0, enemiesCount);
+                var enemy = PossibleEnemies[randomEnemy];
+
+                Vector3 position = new Vector3();
+                position.x = Random.Range(topLeft.x, bottomRight.x);
+                position.y = Random.Range(topLeft.y, bottomRight.y);
+                enemy.GetComponent<Enemy>().target = Target.transform;
+                var genaratedEnemy = Object.Instantiate(enemy, position, m_producer.transform.rotation) as Enemy;
+            }
+        }    
+    }
 }
--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class Map : MonoBehaviour {
 
     public Texture2D Background;
-
+    public GameObject[] PossibleEnemies;
+    public GameObject Player;
+    private GameObject target;
+    private EnemyFactory factory;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        factory = new EnemyFactory(gameObject);
+        factory.PossibleEnemies = PossibleEnemies;
+        
+
+        target = Instantiate(Player);
+        factory.Target = target;
+        InvokeRepeating("Launch", 2, 0.3F);
+    }
+
+    void Launch()
+    {
+        factory.Create(2, new Vector2(-10, -10), new Vector2(10, 10));
+    }
+    
 }
